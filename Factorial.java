3b6630391b4d1b8e543bf7c631a3ac49442dec06
PriyalinD <98249216+PriyalinD@users.noreply.github.com>
2022-01-24 18:23:24 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package week1.day1.assignments;
+
+public class Factorial {
+	
+	public static void main(String arg[])
+	{
+		
+		int number = 5;
+        long fact = 1;
+        for(int i = 1; i <=5; i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println(fact);
+		
+	}
+}
+
---
+++ FibonacciSeries.java
@@ -0,0 +1,33 @@
+package week1.day1.assignments;
+
+public class FibonacciSeries {
+	
+	
+        public static void main(String arg[])
+        {
+        	int count=8,firstNum=0,secondNum=1;
+        	
+        	
+        	for(int i=1;i<=8;++i)
+        	{
+        		System.out.println(firstNum);
+        		
+        		int sum=firstNum +secondNum;
+        		
+                firstNum=secondNum;
+        		
+        		secondNum=sum;
+        		
+        		
+        	}
+        }
+}
+        	
+        	 
+        	 
+        	 
+        	 
+        	 
+        	 
+        	 
+      
